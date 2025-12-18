16ed0bedf37dc0cc051e3cfbdb7150966ee6240e
Victor <viktoroperez@gmail.com>
2025-12-17 22:17:51 -0500

    comit 34

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,7 +9,6 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
-    // log: ["error", "warn"], // opcional
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
