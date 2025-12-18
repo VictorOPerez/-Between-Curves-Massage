79e9980ebb3d7e5d7866cf5412e3f7b7d961316a
Victor <viktoroperez@gmail.com>
2025-12-17 22:08:59 -0500

    comit 33

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,15 @@
 import "server-only";
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
 
-neonConfig.webSocketConstructor = ws;
-
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaNeon({ connectionString });
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+    // log: ["error", "warn"], // opcional
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
